0041a51b20acb063e2021eefa11ec4c992fef17f
Kai Patel <00kaipatel@gmail.com>
2023-07-06 19:53:04 +0100

    build: add prisma as ORM

---
+++ prisma/seed.js
@@ -0,0 +1,59 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+const data = [
+	{
+		firstName: 'Charles',
+		surname: 'Barkley',
+		dateOfBirth: new Date(1963, 2, 20),
+		timesheets: {
+			create: [
+				{
+					year: 2023,
+					month: 5,
+					weeks: {
+						create: [
+							{
+								week: 0,
+								days: {
+									create: [
+										{
+											dayStart: new Date(2023, 7, 3, 8, 0),
+											lunchStart: new Date(2023, 7, 3, 13, 0),
+											lunchEnd: new Date(2023, 7, 3, 14, 0),
+											dayEnd: new Date(2023, 7, 3, 17, 15)
+										}
+									]
+								}
+							}
+						]
+					}
+				}
+			]
+		}
+	}
+];
+
+async function main() {
+	console.log('Started seeding...');
+
+	for (const person of data) {
+		const created = await prisma.person.create({
+			data: person
+		});
+		console.log(`Created ${created.firstName} ${created.surname}`);
+	}
+
+	console.log('Finished seeding');
+}
+
+main()
+	.then(async () => {
+		await prisma.$disconnect();
+	})
+	.catch(async (e) => {
+		console.error(e);
+		await prisma.$disconnect();
+		process.exit(1);
+	});
