1bec2f5b1da489975db59e0d7b75d1738354a9e6
Kai Patel <00kaipatel@gmail.com>
2023-07-07 16:01:33 +0100

    fix some git stuff

--- src/routes/person/[id]/+page.server.ts
+++ src/routes/person/[id]/+page.server.ts
@@ -1,10 +1,19 @@
-import { getDaysWorked } from '$lib/mockdb';
+import prisma from '$lib/prisma';
 import type { PageServerLoad } from './$types';
 
 export const load = (async ({ params }) => {
-	const data = getDaysWorked(parseInt(params.id, 10));
+	// const data = getDaysWorked(parseInt(params.id, 10));
+
+	const weeks = await prisma.month.findMany({
+		where: {
+			personId: params.id
+		},
+		select: {
+			weeks: true
+		}
+	});
 
 	return {
-		data
+		// data
 	};
 }) satisfies PageServerLoad;
