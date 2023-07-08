59b6e4b4fe29904de1b53a27afed5d111371737e
Kai Patel <00kaipatel@gmail.com>
2023-07-08 14:52:38 +0100

    feat: create save action and get form input

--- src/routes/person/[id]/+page.server.ts
+++ src/routes/person/[id]/+page.server.ts
@@ -1,5 +1,6 @@
 import prisma from '$lib/prisma';
 import type { Actions, PageServerLoad } from './$types';
+import chunk from 'lodash/chunk';
 
 export const load = (async ({ params }) => {
 	// const data = getDaysWorked(parseInt(params.id, 10));
@@ -25,3 +26,24 @@ export const load = (async ({ params }) => {
 		days
 	};
 }) satisfies PageServerLoad;
+
+export const actions = {
+	save: async (event) => {
+		const formData = await event.request.formData();
+		const body = [...formData];
+		const inputs = chunk(body, 7);
+
+		const days = [];
+
+		for (let i = 0; i < 7; i++) {
+			days.push({
+				dayStart: inputs[0][i][1],
+				lunchStart: inputs[1][i][1],
+				lunchEnd: inputs[2][i][1],
+				dayEnd: inputs[3][i][1]
+			});
+		}
+
+		console.log(days);
+	}
+} satisfies Actions;
--- src/routes/person/[id]/+page.svelte
+++ src/routes/person/[id]/+page.svelte
@@ -31,8 +31,6 @@
 		}
 	});
 
-	function save() {}
-
 	function prevWeek() {
 		days = subtractWeek(days);
 	}
@@ -50,96 +48,105 @@
 	<Sidebar />
 	<div class="p-0 sm:ml-64 h-screen bg-slate-100">
 		<h1 class="pt-2 px-4 text-2xl font-bold">{`${data.name.firstName} ${data.name.surname}`}</h1>
-		<div class="flex p-4 w-full h-fit">
-			<table class="w-full border-collapse shadow-md table-fixed md:table-auto bg-white">
-				<thead>
-					<tr class="border-b table-row">
-						<th class="border-r bg-slate-500" />
-						{#each days as day}
-							<th class="px-4 py-2 font-semibold text-lg">
-								{day.format('dd DD/MM')}
-							</th>
-						{/each}
-					</tr>
-				</thead>
-				<tbody>
-					<tr class="border-t">
-						<td class="text-center font-semibold text-xs lg:text-lg">Day Start</td>
-						{#each days as day}
-							<td class="py-2 px-4 text-center border-l border-r hover:shadow-inner">
-								<input
-									bind:value={startedValues[day.format('YYYY-MM-DD')]}
-									type="time"
-									class="w-full h-full"
-								/>
-							</td>
-						{/each}
-					</tr>
-					<tr class="border-t">
-						<td class="text-center font-semibold text-xs lg:text-lg">Lunch Start</td>
-						{#each days as day}
-							<td class="py-2 px-4 text-center border-l border-r hover:shadow-inner">
-								<input
-									bind:value={lunchStartedValues[day.format('YYYY-MM-DD')]}
-									type="time"
-									class="w-full h-full"
-								/>
-							</td>
-						{/each}
-					</tr>
-					<tr class="border-t">
-						<td class="text-center font-semibold text-xs lg:text-lg">Lunch End</td>
-						{#each days as day}
-							<td class="py-2 px-4 text-center border-l border-r hover:shadow-inner">
-								<input
-									bind:value={lunchEndedValues[day.format('YYYY-MM-DD')]}
-									type="time"
-									class="w-full h-full"
-								/>
-							</td>
-						{/each}
-					</tr>
-					<tr class="border-t">
-						<td class="text-center font-semibold text-xs lg:text-lg">Day End</td>
-						{#each days as day}
-							<td class="py-2 px-4 text-center border-l border-r hover:shadow-inner">
-								<input
-									bind:value={endedValues[day.format('YYYY-MM-DD')]}
-									type="time"
-									class="w-full h-full"
-								/>
-							</td>
-						{/each}
-					</tr>
-				</tbody>
-			</table>
-		</div>
-		<div class="p-2 h-fit w-full flex justify-center gap-4">
-			<button
-				on:click={prevWeek}
-				class="text-white bg-zinc-600 rounded shadow-lg font-semibold text-lg py-2 px-4 hover:bg-zinc-700"
-			>
-				Previous Week
-			</button>
-			<button
-				on:click={thisWeek}
-				class="bg-orange-400 rounded shadow-lg font-semibold text-lg py-2 px-4 hover:bg-orange-500"
-			>
-				Current Week
-			</button>
-			<button
-				on:click={nextWeek}
-				class="text-white bg-zinc-600 rounded shadow-lg font-semibold text-lg py-2 px-4 hover:bg-zinc-700"
-			>
-				Next Week
-			</button>
-		</div>
-		<div class="p-2 h-fit w-full flex justify-center gap-4">
-			<button
-				on:click={save}
-				class="bg-red-400 rounded shadow-lg font-semibold text-lg py-2 px-4 hover:bg-red-500"
-				>Save</button
-			>
-		</div>
+		<form method="POST" action="?/save">
+			<div class="flex p-4 w-full h-fit">
+				<table class="w-full border-collapse shadow-md table-fixed md:table-auto bg-white">
+					<thead>
+						<tr class="border-b table-row">
+							<th class="border-r bg-slate-500" />
+							{#each days as day}
+								<th class="px-4 py-2 font-semibold text-lg">
+									{day.format('dd DD/MM')}
+								</th>
+							{/each}
+						</tr>
+					</thead>
+					<tbody>
+						<tr class="border-t">
+							<td class="text-center font-semibold text-xs lg:text-lg">Day Start</td>
+							{#each days as day}
+								<td class="py-2 px-4 text-center border-l border-r hover:shadow-inner">
+									<input
+										bind:value={startedValues[day.format('YYYY-MM-DD')]}
+										type="time"
+										class="w-full h-full"
+										name="dayStart"
+									/>
+								</td>
+							{/each}
+						</tr>
+						<tr class="border-t">
+							<td class="text-center font-semibold text-xs lg:text-lg">Lunch Start</td>
+							{#each days as day}
+								<td class="py-2 px-4 text-center border-l border-r hover:shadow-inner">
+									<input
+										bind:value={lunchStartedValues[day.format('YYYY-MM-DD')]}
+										type="time"
+										class="w-full h-full"
+										name="lunchStart"
+									/>
+								</td>
+							{/each}
+						</tr>
+						<tr class="border-t">
+							<td class="text-center font-semibold text-xs lg:text-lg">Lunch End</td>
+							{#each days as day}
+								<td class="py-2 px-4 text-center border-l border-r hover:shadow-inner">
+									<input
+										bind:value={lunchEndedValues[day.format('YYYY-MM-DD')]}
+										type="time"
+										class="w-full h-full"
+										name="lunchEnd"
+									/>
+								</td>
+							{/each}
+						</tr>
+						<tr class="border-t">
+							<td class="text-center font-semibold text-xs lg:text-lg">Day End</td>
+							{#each days as day}
+								<td class="py-2 px-4 text-center border-l border-r hover:shadow-inner">
+									<input
+										bind:value={endedValues[day.format('YYYY-MM-DD')]}
+										type="time"
+										class="w-full h-full"
+										name="dayEnd"
+									/>
+								</td>
+							{/each}
+						</tr>
+					</tbody>
+				</table>
+			</div>
+			<div class="p-2 h-fit w-full flex justify-center gap-4">
+				<button
+					type="button"
+					on:click={prevWeek}
+					class="text-white bg-zinc-600 rounded shadow-lg font-semibold text-lg py-2 px-4 hover:bg-zinc-700"
+				>
+					Previous Week
+				</button>
+				<button
+					type="button"
+					on:click={thisWeek}
+					class="bg-orange-400 rounded shadow-lg font-semibold text-lg py-2 px-4 hover:bg-orange-500"
+				>
+					Current Week
+				</button>
+				<button
+					type="button"
+					on:click={nextWeek}
+					class="text-white bg-zinc-600 rounded shadow-lg font-semibold text-lg py-2 px-4 hover:bg-zinc-700"
+				>
+					Next Week
+				</button>
+			</div>
+			<div class="p-2 h-fit w-full flex justify-center gap-4">
+				<button
+					type="submit"
+					class="bg-red-400 rounded shadow-lg font-semibold text-lg py-2 px-4 hover:bg-red-500"
+					>Save</button
+				>
+			</div>
+		</form>
 	</div>
 </body>
