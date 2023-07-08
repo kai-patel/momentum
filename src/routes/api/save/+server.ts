961972eb93e9e4076203b22543aa102c164ba4d9
Kai Patel <00kaipatel@gmail.com>
2023-07-08 14:52:19 +0100

    fix: remove save API endpoint

--- src/routes/api/save/+server.ts
+++
@@ -1,6 +0,0 @@
-import type { RequestEvent, RequestHandler } from './$types';
-
-export const POST = ((event: RequestEvent) => {
-	const params = event.params;
-	return new Response(event.url.toString());
-}) satisfies RequestHandler;
