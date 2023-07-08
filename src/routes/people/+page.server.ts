cdff120cb4de6597fed92cb03df39b857839b58d
Kai Patel <00kaipatel@gmail.com>
2023-07-08 19:16:40 +0100

    feat: addPerson action connected to db

--- src/routes/people/+page.server.ts
+++ src/routes/people/+page.server.ts
@@ -1,5 +1,6 @@
 import type { Actions, PageServerLoad } from './$types';
 import prisma from '$lib/prisma';
+import moment from 'moment';
 
 export const load = (async ({}) => {
 	const data = await prisma.person.findMany({
@@ -19,6 +20,23 @@ export const load = (async ({}) => {
 
 export const actions = {
 	addPerson: async (event) => {
-		console.log(...(await event.request.formData()));
+		const formData = await event.request.formData();
+		const body = [...formData];
+
+		console.log(body);
+
+		const dob = moment(body[2][1].toString(), 'YYYY-MM-DD', true).toDate();
+
+		const createPerson = await prisma.person.create({
+			data: {
+				firstName: body[0][1].toString(),
+				surname: body[1][1].toString(),
+				dateOfBirth: dob
+			}
+		});
+
+		console.log('CREATED PERSON ----', createPerson);
 	}
+
+    // TODO Remove person
 } satisfies Actions;
