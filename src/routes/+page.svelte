a8ced5e0a7e7adcdcdf490428131832401be5c2f
Kai Patel <00kaipatel@gmail.com>
2023-07-10 21:11:00 +0100

    fix: make login button actually login

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,7 @@
+<script lang="ts">
+	import { signIn } from '@auth/sveltekit/client';
+</script>
+
 <div
 	class="flex flex-col h-screen w-screen items-center place-content-center bg-gradient-to-b from-purple-800 to-red-600 gap-2"
 >
@@ -7,10 +11,10 @@
 	<p class="select-none overline font-bold text-white text-lg text-center drop-shadow-lg">
 		Modern time management
 	</p>
-	<a
-		href="/home"
+	<button
 		class="cursor-pointer bg-blue-400 rounded shadow-lg font-semibold text-lg py-2 px-4"
+		on:click={() => signIn()}
 	>
 		Login
-	</a>
+	</button>
 </div>
